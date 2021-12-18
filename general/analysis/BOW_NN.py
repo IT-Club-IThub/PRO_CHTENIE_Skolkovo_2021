1c4620e07e6d0190c9d8fb03ee4f670139af654b
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-18 19:58:13 +0300

    Использую наследование, чтоб убежать от повторного года в генерации нейросетей

--- general/analysis/BOW_NN.py
+++ general/analysis/BOW_NN.py
@@ -1,36 +1,9 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-import numpy as np
+from NN import NN
 from tensorflow.python.keras.layers.core import Dropout
 
-class BOW_NN:
-  def __init__(self, dataset_path: str, debug: bool, batch_size: int, epochs:int=15):
-    self.dataset_path = dataset_path
-    self.debug = debug
-    self.batch_size = batch_size
-
-    dataset = np.load(dataset_path)
-    self.xTrain = dataset['xTrain']
-    self.yTrain = dataset['yTrain']
-    self.xTest = dataset['xTest']
-    self.yTest = dataset['yTest']
-    self.xVal = dataset['xVal']
-    self.yVal = dataset['yVal']
-  
-    if(self.debug):
-      print(self.xTrain.shape)
-      print(self.yTrain.shape)
-      print(self.xTest.shape)
-      print(self.yTest.shape)
-      print(self.xVal.shape)
-      print(self.yVal.shape)
-
-    filename = dataset_path.split('/')[-1]
-    self.numWords = int(filename.split('_')[2])
-    # self.xLen = int(filename.split('_')[5])
-    self.epochs = epochs
-
-
+class BOW_NN(NN):
   def compile(self):
     self.model = Sequential()
     self.model.add(Dense(512, input_dim=self.numWords, activation="relu"))
@@ -50,20 +23,3 @@ class BOW_NN:
                                 validation_data=(self.xVal, self.yVal),
                                 use_multiprocessing=True,
                                 verbose=int(self.debug))
-
-  def check(self):
-    rightAnswer = [0,0]
-    totalAnswer = [0,0]
-    currPred = self.model.predict(self.xTest)
-    currOut = np.argmax(currPred, axis=1)
-    yOut = np.argmax(self.yTest, axis=1)
-    for i in range(len(yOut)):
-      predictA = currOut[i]
-      rightA   = yOut[i]
-      totalAnswer[rightA] += 1
-      if predictA == rightA:
-        rightAnswer[rightA] += 1
-
-    # print(f"Точность распознавания текстов на {self.dataset_path}")
-    for i in range(2):
-      print("{:12s}: {:3d} из {:3d} - {:3.2f}%".format(str(i), rightAnswer[i], totalAnswer[i], (rightAnswer[i]/totalAnswer[i]*100)))
--- general/analysis/main.py
+++ general/analysis/main.py
@@ -3,27 +3,27 @@ from BOW_NN import BOW_NN
 from Embedding_NN import Embedding_NN
 from RNN_NN import RNN_NN
 import os
+from datetime import datetime
 
 def main():
-    datasets = []
-    for l in sorted(os.listdir('../datasets/')):
-        if("BOW" not in l.upper()):
-            datasets.append(l)
-
-    for dataset_path in datasets[::-1]:
-        print("--------------" + dataset_path.upper() + "--------------")
-        dataset = '../datasets/' + dataset_path
-        debug = False
-        for step in dataset_path.split('_')[6].replace('(', '').replace(')', '').split(','):
-            step = int(step)
-            print(f'{step=}')
-            model = LSTM_NN(dataset, debug=debug, batch_size=128, step=step)
-            model.compile()
-            model.fit()
-            model.check()
+    dataset_path = "dataset_numWords_48131_xLen_300_step_(150, 35)_.npz"
+    print("--------------" + dataset_path.upper() + "--------------")
+    dataset = '../datasets/' + dataset_path
+    debug = True
+    step = 150
+    model = LSTM_NN(dataset, debug=debug, batch_size=128, step=step)
+    model.compile()
+    # model.model.load_weights('title2')
+    while (True):
+        model.fit(2)
+        model.check()
+        # a = input('y or s?:')
+        # if a == "s":
+        #     model.model.save_weights('title2')
+        # if a == "y":
+        #     break
         
        
 
 if __name__ == "__main__":
     main()
-  
