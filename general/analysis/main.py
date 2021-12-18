20b18b0f4d7ecf6ad139b37bfd3d91d84b4bb767
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-19 00:30:01 +0300

    Future Foundation пинг

--- general/analysis/main.py
+++ general/analysis/main.py
@@ -1,3 +1,4 @@
+from json.encoder import JSONEncoder
 from LSTM_NN import LSTM_NN
 from BOW_NN import BOW_NN
 from Embedding_NN import Embedding_NN
@@ -9,7 +10,7 @@ from get_xTest import get_xTest
 
 def main():
     print(os.listdir('..'))
-    dataset_path = "NEWNEWdataset_numWords_49208_xLen_300_step_300_.npz"
+    dataset_path = "NEWNEWdataset_numWords_52628_xLen_300_step_300_.npz"
     print("--------------" + dataset_path.upper() + "--------------")
     dataset = '../datasets/' + dataset_path
     debug = True
@@ -17,26 +18,23 @@ def main():
     model = LSTM_NN(dataset, debug=debug, batch_size=128, step=step)
     model.compile()
     # model.model.load_weights('title2')
-    model.fit(1)
+    model.fit(2)
 
     # открываем файл с id эссе и ответами
-    with open('../../Satellites/test/test_task.json') as f_list:
-        data = json.load(f_list)
+    # file = open('../../Satellites/test/test_task.json')
+    # data = json.load(file)
+    # file.close()
+    # length_data = len(data)
+    # for i in range(length_data):
+    #     xTest = get_xTest(data[i])
+    #     pred = model.model.predict(xTest)
+    #     data[i]["answer"] = int(pred[0][0] > pred[0][1])
+    #     print(i, '/', length_data)
 
-        for i in range(len(data)):
-            elem = data[i]
-            xTestWrode = get_xTest(elem)
-            pred = model.model.predict(xTestWrode)
-            print(pred)
+    # file = open('../../Satellites/test/test_task_WE_ARE_THE_CEMPION.json', 'w')
+    # json.dump(data, file)
+    # file.close()
 
-            # elem["answer"] = model.check(get_xTest)
-            
-        # a = input('y or s?:')
-        # if a == "s":
-        #     model.model.save_weights('title2')
-        # if a == "y":
-        #     break
-        
        
 
 if __name__ == "__main__":
